75751c2997c9227dc3a2a4b06ff2a1203195bc83
nknight01 <Nknight01@rio.edu>
2026-05-08 00:56:42 -0400

    Fix broken order.js debugging issue in Week 4

---
+++ week04/order.js
@@ -0,0 +1,29 @@
+function calculateTotal(price, quantity) {
+  return price * quantity;
+}
+
+function applyDiscount(total) {
+  const discount = 20; // fixed discount value
+
+  if (total > 100) {
+    return total - discount;
+  }
+  return total;
+}
+
+function processOrder(price, quantity) {
+  const total = calculateTotal(price, quantity);
+  const discounted = applyDiscount(total);
+  return discounted.toFixed(2);
+}
+
+ 
+
+module.exports = {
+  calculateTotal,
+  applyDiscount,
+  processOrder
+};
+ 
+
+console.log(processOrder(25, 5));
